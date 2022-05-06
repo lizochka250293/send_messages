98cf8a662347627226ee4eece0a55418527e2915
vera <lerika250293@mail.ru>
2022-05-06 13:42:40 +0300

    do send_message

---
+++ main.py
@@ -0,0 +1,15 @@
+import pywhatkit
+from flask import Flask, request
+
+def send_message():
+    list = ['+###########']
+    message = 'Hello'
+    for i in list:
+        pywhatkit.sendwhatmsg(i, "Проверяла работу приложения", 13, 27)
+    # pywhatkit.sendwhatmsg_to_group_instantly(phone=mobile, message=message)
+
+def main():
+    send_message()
+
+if __name__ == '__main__':
+    main()
